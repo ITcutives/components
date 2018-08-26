ac6e8060f972128adb000a76e872118dfec7936a
Riddhi Tilara <riddhi@itcutives.com>
2018-08-26 16:51:33 +1000

    button component

--- src/components/pc-fab/pc-fab.tsx
+++ src/components/pc-fab/pc-fab.tsx
@@ -1,15 +1,21 @@
-import { Component } from '@stencil/core';
+import {Component, Prop} from '@stencil/core';
 
 @Component({
   tag: 'pc-fab',
   styleUrl: 'pc-fab.scss'
 })
 export class PcFab {
+  @Prop() bgClass: string = 'primary';
+
+  getClass() {
+    return `btn ${this.bgClass} btn-round`;
+  }
+
   render() {
     return (
-      <div class="pc-fab">
-        pc-fab
-      </div>
+      <button class={this.getClass()}>
+        <slot></slot>
+      </button>
     );
   }
 }
